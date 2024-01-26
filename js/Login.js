bbaaeed501fada9de38864f4ea962a085d704446
Ingrid <109824614+chacha-smooth@users.noreply.github.com>
2024-01-26 12:38:01 -0500

    Worked on the login script.

---
+++ js/Login.js
@@ -0,0 +1,41 @@
+
+
+    const button = document.getElementById("login-btn");
+
+	button.addEventListener("click", doLogin);
+
+
+function doLogin(){
+	let login = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+
+	let tmp = {login:login,password:password};
+
+	
+	//console.log(tmp);
+
+	let jsonPayload = JSON.stringify( tmp );
+	urlBase = "http://159.203.170.88/LAMPAPI"
+	let url = urlBase + '/Login.php' ;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let jsonObject = JSON.parse( xhr.responseText );
+                console.log(jsonObject);
+				window.location.href = "landing-page.html";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		console.log(err);
+	}
+}
