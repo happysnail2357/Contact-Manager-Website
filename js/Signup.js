c40fcc3ee3161819812f4f9a60500fa6e4b40114
Paul P <paulpuhnaty@knights.ucf.edu>
2024-02-09 20:52:51 -0500

    Added field validation

--- js/Signup.js
+++ js/Signup.js
@@ -10,9 +10,86 @@ let jsonObject = null;
 
 function doLogin(){
 	let login = document.getElementById("username").value;
-	let password = document.getElementById("pass").value;
+	let password = document.getElementById("password").value;
+	let conf_password = document.getElementById("confirm-password").value;
     let firstName = document.getElementById("f-name").value;
     let lastName = document.getElementById("l-name").value;
+	
+	// Clear error messages if any
+	document.getElementById("username-error").textContent = "";
+	document.getElementById("password-error").textContent = "";
+	document.getElementById("f-name-error").textContent = "";
+	document.getElementById("l-name-error").textContent = "";
+	document.getElementById("username").style.borderColor = "#ccc";
+	document.getElementById("password").style.borderColor = "#ccc";
+	document.getElementById("confirm-password").style.borderColor = "#ccc";
+	document.getElementById("f-name").style.borderColor = "#ccc";
+	document.getElementById("l-name").style.borderColor = "#ccc";
+	
+	let missingField = false;
+	
+	// Check for empty fields
+	if (login == "")
+	{
+		document.getElementById("username-error").textContent = "Username required";
+		document.getElementById("username").style.borderColor = "red";
+		
+		missingField = true;
+	}
+	if (password == "")
+	{
+		document.getElementById("password-error").textContent = "Password required";
+		document.getElementById("password").style.borderColor = "red";
+		document.getElementById("confirm-password").style.borderColor = "red";
+		
+		missingField = true;
+	}
+	if (firstName == "")
+	{
+		document.getElementById("f-name-error").textContent = "First Name required";
+		document.getElementById("f-name").style.borderColor = "red";
+		
+		missingField = true;
+	}
+	if (lastName == "")
+	{
+		document.getElementById("l-name-error").textContent = "Last Name required";
+		document.getElementById("l-name").style.borderColor = "red";
+		
+		missingField = true;
+	}
+	
+	if (missingField)
+		return;
+	
+	// Password minimum requirements
+	if (password.length < 8)
+	{
+		document.getElementById("password-error").textContent = "Password must have at least 8 characters";
+		document.getElementById("password").style.borderColor = "red";
+		
+		return;
+	}
+	if (password.search(/[0-9]/) == -1 || password.search(/[A-Z]/) == -1 || password.search(/[a-z]/) == -1)
+	{
+		document.getElementById("password-error").textContent = "Password must have a lowercase letter, an UPPERCASE letter, and a number";
+		document.getElementById("password").style.borderColor = "red";
+		
+		return;
+	}
+	
+	// Passwords must match
+	if (password != conf_password)
+	{
+		document.getElementById("password-error").textContent = "Passwords must match";
+		document.getElementById("password").style.borderColor = "red";
+		document.getElementById("confirm-password").style.borderColor = "red";
+		
+		return;
+	}
+	
+	
+	
 	
 	let temp = {
 		'login':login,
@@ -23,7 +100,7 @@ function doLogin(){
 	
 	let url ="https://cardboardmc.com/LAMPAPI/Register.php";
 
-  postData(url, temp).then((data) => 
+	postData(url, temp).then((data) => 
 	{
 		if(data.error != null){
 			jsonObject=data;
